d7520668f77b643bfacc2f1181045f53de59bf2b
EdwardMelendezM <192666@unsaac.edu.pe>
2023-06-15 12:45:41 -0500

    adding SettingModal and api to update user's informaction

---
+++ app/api/settings/route.ts
@@ -0,0 +1,39 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import { NextResponse } from "next/server";
+import prisma from '@/app/libs/prismadb';
+
+
+export async function POST(
+  request:Request
+){
+  try {
+    const currentUser = await getCurrentUser();
+    const body = await request.json();
+    const {
+      name,
+      image
+    } = body;
+
+    if(!currentUser?.id){
+      return new NextResponse('Unauthorized',{status:401})
+    }
+
+    const updateUser = await prisma.user.update({
+      where:{
+        id:currentUser.id
+      },
+      data:{
+        image:image,
+        name:name
+      }
+    })
+
+    return NextResponse.json(updateUser)
+
+  } catch (error:any) {
+    console.log(error,'ERROR_SETTING');
+    return new NextResponse('Internal Error',{status:500})
+  }
+
+
+}
